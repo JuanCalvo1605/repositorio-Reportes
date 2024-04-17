ff0e8a215393d04d37f7a43bb68733b6740129ff
Juan Sebastian des. Calvo <desarrollador@grupoandes.com>
2024-04-17 07:00:51 -0500

    primer commit donde se inicio el proyecto

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="home-page">
+        <h1>Principal</h1>
+        <p>Estas en el menu Principal</p>
+    </main>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>./home.vue
---
+++ src/views/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="home-page">
+        <h1>About</h1>
+        <p>Estas en el about Principal</p>
+    </main>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
